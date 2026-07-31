3c80caed686342b74d14f5e73754fd7b650d1165
Abd. Rasyid <a6drasyid@gmail.com>
2026-07-31 12:16:07 +0800

    Store created_at using WITA timezone

--- server/config/database.js
+++ server/config/database.js
@@ -7,9 +7,6 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
-  // Tambahkan ini
-  timezone: "+08:00",
-
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
